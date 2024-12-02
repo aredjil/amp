9a0f191408ceb26242dda5c8cf90d452afe2b840
aredjil <bellred499@gmail.com>
2024-12-02 16:31:54 +0100

    added type hinting to the class

--- amp/amp.py
+++ amp/amp.py
@@ -4,32 +4,32 @@ import numpy as np
 # TODO: Update variable and function names. 
 # TODO: Use numpy arrays instead of lists in the SER.  
 class amp:
-  def __init__(self, L, B):
+  def __init__(self, L:int, B:int):
     np.random.seed(0)
-    self.L = L
-    self.B = B
-    self.N = L * B
-    self.x = self.gen_ss()
+    self.L:int = L
+    self.B:int = B
+    self.N:int = L * B
+    self.x:int = self.gen_ss()
 
-  def awgn_ch(self, snr):
-    self.snr = snr
-    self.M = int((self.N * np.log2(self.B)) / (self.R * self.B))
-    std = 1 / np.sqrt(snr)
-    x_gt = self.x.reshape(self.N)
-    F = np.random.normal(0, 1/np.sqrt(self.L), (self.M, self.N))
-    self.F = F
-    noise = std * np.random.randn(self.M)
-    y = (F @ x_gt) + noise
+  def awgn_ch(self, snr:int)->np.ndarray:
+    self.snr:int = snr
+    self.M:int = int((self.N * np.log2(self.B)) / (self.R * self.B))
+    std:float = 1 / np.sqrt(snr)
+    x_gt:np.ndarray = self.x.reshape(self.N)
+    F:np.ndarray = np.random.normal(0, 1/np.sqrt(self.L), (self.M, self.N))
+    self.F:np.ndarray = F
+    noise:np.ndarray = std * np.random.randn(self.M)
+    y:np.ndarray = (F @ x_gt) + noise
     return y
 
-  def gen_ss(self):
-    x = np.zeros((self.L, self.B))
+  def gen_ss(self)->np.ndarray:
+    x:np.ndarray = np.zeros((self.L, self.B))
     for arr in x:
-      randy = np.random.randint(0, self.B)
+      randy:np.ndarray = np.random.randint(0, self.B)
       arr[randy] = 1
     return x
 
-  def g(self, z, y, V, snr):
+  def g(self, z:np.ndarray, y:np.ndarray, V:np.ndarray, snr:int)->np.ndarray:
     '''
     A function to be described
     Args:
@@ -40,11 +40,11 @@ class amp:
     Returns:
     B (numpy.array): ?
     '''
-    var = 1 / snr
-    B = (y - z) / (V + var)
+    var:float = 1 / snr
+    B:np.ndarray = (y - z) / (V + var)
     return B
 
-  def dg(self, V, snr):
+  def dg(self, V:np.ndarray, snr:int)->np.ndarray:
     '''
     A function to be described
     Args:
@@ -54,22 +54,22 @@ class amp:
     Returns:
     A (numpy.array): ?
     '''
-    var = 1 / snr
-    A = 1 / (V + var)
+    var:int = 1 / snr
+    A:np.ndarray = 1 / (V + var)
     return A
 
-  def denoising_a(self, x_hat, v, sigma, r, L, B, c=1):
+  def denoising_a(self, x_hat, v, sigma, r, L, B, c=1)->tuple[np.ndarray, np.ndarray]:
 
-    N = int(L * B)
-    new_sigma = sigma.copy().reshape(L, B)
-    new_r = r.copy().reshape(L, B)
-    x_hat = x_hat.reshape(L, B)
-    v = v.reshape(L, B)
+    N:int = int(L * B)
+    new_sigma:np.ndarray = sigma.copy().reshape(L, B)
+    new_r:np.ndarray = r.copy().reshape(L, B)
+    x_hat:np.ndarray = x_hat.reshape(L, B)
+    v:np.ndarray = v.reshape(L, B)
     for l in range(L):
         x_hat[l, :] = c* np.exp((-c * (c-2*new_r[l, :]))/(2*new_sigma[l, :]**2)) / np.sum(np.exp((-c * (c-2*new_r[l, :]))/(2*new_sigma[l, :]**2)))
         v[l,:] = x_hat[l,:] * (1-x_hat[l, :])
-    x_hat  = x_hat.reshape(N)
-    v = v.reshape(N)
+    x_hat:np.ndarray  = x_hat.reshape(N)
+    v:np.ndarray = v.reshape(N)
     return x_hat, v
 
   def amplify(self, arr, c=1):
@@ -82,14 +82,14 @@ class amp:
     Returns:
         arr_new (numpy.array) : Sparse suprposition code (L * B)
         """
-    row_idx = np.arange(self.L)
-    col_idx = self.get_argmax(arr)
-    arr = arr.reshape(self.L, self.B)
-    arr_new = np.zeros((self.L, self.B))
+    row_idx:int = np.arange(self.L)
+    col_idx:int = self.get_argmax(arr)
+    arr:np.ndarray = arr.reshape(self.L, self.B)
+    arr_new:np.ndarray = np.zeros((self.L, self.B))
     arr_new[row_idx, col_idx] = c
     return arr_new
 
-  def section_error_rate(self, x_gt, x_hat, c=1):
+  def section_error_rate(self, x_gt, x_hat, c=1)->list[float]:
     """
     Calculates Section Error Rate
 
@@ -102,9 +102,9 @@ class amp:
     Returns:
         ser (float) : Section Error Rate (fraction of the wrongly re-constructed sections)
     """
-    x_hat = self.amplify(x_hat, c=1)
-    x_gt = x_gt.reshape(self.L, self.B)
-    count = 0
+    x_hat:np.ndarray = self.amplify(x_hat, c=1)
+    x_gt:np.ndarray = x_gt.reshape(self.L, self.B)
+    count:int = 0
     for row1, row2 in zip(x_hat, x_gt):
         if all(row1 == row2):
             count += 0
@@ -112,7 +112,7 @@ class amp:
             count += 1
     ser = count / self.L
     return ser
-  def get_argmax(self, arr):
+  def get_argmax(self, arr:np.ndarray)->int:
     """
     Retrive the indices of the maximum argument of a L * B array
 
@@ -124,11 +124,11 @@ class amp:
         idx (numpy.array) : array of indices
 
     """
-    arr = arr.reshape(self.L, self.B)
-    idx = np.argmax(arr, axis=1)
+    arr:np.ndarray = arr.reshape(self.L, self.B)
+    idx:int = np.argmax(arr, axis=1)
     return idx
 
-  def decode(self, snr, R=1.3, t_max=25, ep=10**-8, c=1):
+  def decode(self, snr:int, R:float=1.3, t_max:int=25, ep:int=10**-8, c:int=1)->list[float]:
     """
     Approximate message decoder for sparse superposition codes as defined in https://arxiv.org/abs/1403.8024
 
@@ -149,35 +149,35 @@ class amp:
         section_error (numpy.array) : Section-Error-Rate at each
         mse (numpy.array)           : Mean-Square-Error per section
     """
-    self.R = R
-    y = self.awgn_ch(snr)
+    self.R:int = R
+    y:np.ndarray = self.awgn_ch(snr)
     # Initialization
-    delta = 1 + ep
-    t = 0
+    delta:float = 1 + ep
+    t:int = 0
     # AMP estimate of the signal
-    x_hat = np.zeros(self.N)
+    x_hat:np.ndarray = np.zeros(self.N)
     # AMP estimate of the variances per component
-    v = np.ones(self.N)/(snr * self.B)
+    v:np.ndarray = np.ones(self.N)/(snr * self.B)
     #v = np.zeros(N)
     # Variances of the estimation of messages before taking the prior into account
-    V = np.zeros(self.M)
-    z = y.copy()
-    ser = []
+    V:np.ndarray = np.zeros(self.M)
+    z:np.ndarray = y.copy()
+    ser:list[float] = []
     ser.append(1)
     while t < t_max and delta > ep:
         # Here I just created a copy of he intilized arrays to use them as old instances (i.e values at t-1 when calculatin values at time step t)
-        V_old = V.copy()
-        z_old = z.copy()
-        x_hat_old =x_hat.copy()
+        V_old:np.ndarray = V.copy()
+        z_old:np.ndarray = z.copy()
+        x_hat_old:np.ndarray =x_hat.copy()
 
         # AMP starts here
-        V = self.F **2 @ v
-        z = (self.F @ x_hat) - V * self.g(z_old, y, V_old, snr)
+        V:np.ndarray = self.F **2 @ v
+        z:np.ndarray = (self.F @ x_hat) - V * self.g(z_old, y, V_old, snr)
 
-        sigma = 1 / np.sqrt(self.dg(V, snr) @ self.F**2)
+        sigma:np.ndarray = 1 / np.sqrt(self.dg(V, snr) @ self.F**2)
 
         # Estimation of messages before taking the prior into account
-        r = x_hat_old + sigma ** 2 * (self.g(z, y, V, snr) @ self.F)
+        r:np.ndarray = x_hat_old + sigma ** 2 * (self.g(z, y, V, snr) @ self.F)
         # Creating copies of sigma and r arrays and reshaping them in order to use them in the denoisin function and update the estimates
         x_hat, v = self.denoising_a(x_hat, v, sigma, r, self.L , self.B, c)
         delta = (1/self.N)* np.sum(np.square(x_hat-x_hat_old))
